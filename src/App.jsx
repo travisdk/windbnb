0d1bd0e204ea03236347c6f0c1154e57a5b98a05
Henrik Juhl <henrik.juhl_dk@hotmail.com>
2023-05-24 12:53:22 +0200

    basic setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "../sass/App.scss";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="container text-center my-2">
+        <h2 className="display-4 m-3 bg-secondary text-center text-light p-3 rounded-2 shadow-sm">
+          Welcome to WindBnB !
+        </h2>
+        <button className="btn btn-primary px-4 py-3 text-white fw-bolder">
+          My First Button
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
